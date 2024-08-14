1b9f68a8ec89e20bc4c72af3e96869813417f1f1
Madhurjya Roy <m@mroy.me>
2024-08-14 13:16:24 +0530

    refactor: change project structure

---
+++ cmd/dogstatsd-local/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/mroyme/dogstatsd-local/internal/handler"
+	"github.com/mroyme/dogstatsd-local/internal/server"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+)
+
+func main() {
+	host := flag.String("host", "0.0.0.0", "bind address")
+	port := flag.Int("port", 8125, "listen port")
+	format := flag.String("format", "stdout", "output format: json|std|raw")
+	rawTags := flag.String("tags", "", "extra tags: comma delimited")
+	flag.Parse()
+
+	extraTags := strings.Split(*rawTags, ",")
+	var msgHandler handler.MsgHandler
+
+	switch *format {
+	case "json":
+		msgHandler = handler.NewJSONDogStatsDMsgHandler(extraTags)
+	case "human":
+		msgHandler = handler.NewHumanDogStatsDMsgHandler(extraTags)
+	default:
+		msgHandler = handler.NewRawDogStatsDMsgHandler()
+	}
+	asyncHandler := handler.NewAsyncMsgHandler(msgHandler, 1000, 10000)
+
+	var wg sync.WaitGroup
+
+	// create a new server and listen on a background goroutine
+	addr := fmt.Sprintf("%s:%d", *host, *port)
+	log.Println("listening over UDP at ", addr)
+	srv := server.NewServer(addr, asyncHandler.Handler)
+	wg.Add(1)
+	go func(srv server.Server) {
+		defer wg.Done()
+		if err := srv.Listen(); err != nil {
+			log.Fatalf(err.Error())
+		}
+	}(srv)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	<-sigCh
+
+	if err := srv.Stop(); err != nil {
+		log.Println(err.Error())
+	}
+	wg.Wait()
+	asyncHandler.Stop()
+}
---
+++ internal/messages/messages.go
@@ -0,0 +1,190 @@
+package messages
+
+import (
+	"bytes"
+	"errors"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type DogStatsDMetricType int
+
+const (
+	GaugeMetricType DogStatsDMetricType = iota
+	CounterMetricType
+	SetMetricType
+	TimerMetricType
+	HistogramMetricType
+)
+
+func (d DogStatsDMetricType) String() string {
+	switch d {
+	case GaugeMetricType:
+		return "gauge"
+	case CounterMetricType:
+		return "counter"
+	case SetMetricType:
+		return "set"
+	case TimerMetricType:
+		return "timer"
+	case HistogramMetricType:
+		return "histogram"
+	}
+	return "unknown"
+}
+
+type DogStatsDMessageType int
+
+const (
+	MetricMessageType DogStatsDMessageType = iota
+	ServiceCheckMessageType
+	EventMessageType
+)
+
+func (d DogStatsDMessageType) String() string {
+	switch d {
+	case MetricMessageType:
+		return "metric"
+	case EventMessageType:
+		return "event"
+	case ServiceCheckMessageType:
+		return "service_check"
+	}
+	return "unknown"
+}
+
+type DogStatsDMetric struct {
+	Namespace     string
+	Name          string
+	MetricData    []byte
+	Timestamp     time.Time
+	MetricType    DogStatsDMetricType
+	RawValue      string
+	FloatValue    float64
+	DurationValue time.Duration
+	Extras        []string
+	Tags          []string
+	SampleRate    float64
+}
+
+func (d DogStatsDMetric) Data() []byte {
+	return d.MetricData
+}
+
+func (d DogStatsDMetric) Type() DogStatsDMessageType {
+	return MetricMessageType
+}
+
+type DogStatsDServiceCheck struct {
+	data []byte
+}
+
+func (d DogStatsDServiceCheck) Data() []byte {
+	return d.data
+}
+
+func (DogStatsDServiceCheck) Type() DogStatsDMessageType {
+	return ServiceCheckMessageType
+}
+
+type DogStatsDMessage interface {
+	Type() DogStatsDMessageType
+	Data() []byte
+}
+
+// ParseDogStatsDMessage parses a DogStatsDMessage, returning the correct message back
+func ParseDogStatsDMessage(buf []byte) (DogStatsDMessage, error) {
+	if bytes.HasPrefix(buf, []byte("_e{")) {
+		return parseDogStatsDEventMessage(buf)
+	}
+	if bytes.HasPrefix(buf, []byte("_sc{")) {
+		return parseDogStatsDServiceCheckMessage(buf)
+	}
+	return parseDogStatsDMetricMessage(buf)
+}
+
+func parseDogStatsDMetricMessage(buf []byte) (DogStatsDMessage, error) {
+	metric := DogStatsDMetric{
+		Timestamp:  time.Now(),
+		Tags:       make([]string, 0),
+		SampleRate: 1.0,
+	}
+
+	// sample message: metric.name:value|type|@sample_rate|#tag1:value,tag2
+	pieces := strings.Split(string(buf), "|")
+	if len(pieces) < 2 {
+		return nil, errors.New("invalid message: missing name, value, or type")
+	}
+
+	addrAndValue := strings.Split(pieces[0], ":")
+	if len(addrAndValue) < 2 {
+		return nil, errors.New("invalid message: missing name and value")
+	}
+
+	namespaceAndName := strings.SplitN(addrAndValue[0], ".", 2)
+	if len(namespaceAndName) > 1 {
+		metric.Namespace = namespaceAndName[0]
+		metric.Name = namespaceAndName[1]
+	} else {
+		metric.Name = namespaceAndName[0]
+	}
+
+	metric.RawValue = addrAndValue[1]
+
+	switch pieces[1] {
+	case "c":
+		metric.MetricType = CounterMetricType
+	case "g":
+		metric.MetricType = GaugeMetricType
+	case "s":
+		metric.MetricType = SetMetricType
+	case "ms":
+		metric.MetricType = TimerMetricType
+	case "h":
+		metric.MetricType = HistogramMetricType
+	default:
+		return nil, errors.New("invalid message: unknown metric type")
+	}
+
+	// all values are stored as a float
+	floatValue, err := strconv.ParseFloat(metric.RawValue, 64)
+	if err != nil {
+		return nil, errors.New("invalid message: invalid value")
+	}
+	metric.FloatValue = floatValue
+
+	if metric.MetricType == TimerMetricType {
+		metric.DurationValue = time.Duration(metric.FloatValue) / time.Millisecond
+	}
+
+	// parse out sample rate, tags and any extras
+	for _, piece := range pieces[2:] {
+		if strings.HasPrefix(piece, "@") {
+			sampleRate, err := strconv.ParseFloat(piece[1:], 64)
+			if err != nil {
+				return nil, errors.New("invalid sample rate")
+			}
+			metric.SampleRate = sampleRate
+			continue
+		}
+
+		if strings.HasPrefix(piece, "#") {
+			tags := strings.Split(piece[1:], ",")
+			metric.Tags = append(metric.Tags, tags...)
+			continue
+		}
+
+		metric.Extras = append(metric.Extras, piece)
+	}
+
+	return metric, nil
+}
+
+func parseDogStatsDEventMessage(_ []byte) (DogStatsDMessage, error) {
+	return nil, errors.New("DogStatsD event messages not supported")
+}
+
+func parseDogStatsDServiceCheckMessage(_ []byte) (DogStatsDMessage, error) {
+	return nil, errors.New("DogStatsD service check messages not supported")
+}
